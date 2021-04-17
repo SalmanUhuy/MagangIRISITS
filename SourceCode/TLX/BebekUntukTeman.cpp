bb75d9bb6ec683ec48c73bc51bb338c48007559d
Salman <81957113+SalmanUhuy@users.noreply.github.com>
2021-04-17 20:52:39 +0700

    Create BebekUntukTeman.cpp

---
+++ SourceCode/TLX/BebekUntukTeman.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n,m;
+    cin >> n;
+    cin >> m;
+ 
+    cout << "masing-masing " << n/m << endl;
+    cout << "bersisa " << n%m << endl;
+    return 0;
+}
